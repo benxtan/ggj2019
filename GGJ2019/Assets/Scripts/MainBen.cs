c1fbccf908ece64a15d4c177552d895060b0cfcd
benxtan <benxtan@gmail.com>
2019-01-25 22:36:46 +0000

    made some changes

---
+++ GGJ2019/Assets/Scripts/MainBen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBen : MonoBehaviour
+{
+    public GameObject Tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject newTile = GameObject.Instantiate(Tile);
+            newTile.transform.position = new Vector3(0.64f * i, -0.33f * i, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

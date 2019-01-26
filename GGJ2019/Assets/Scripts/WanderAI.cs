e3ecab5b705ee244d8308ae9aa1d60747eb7e198
Daniel <dev@salvagelabs.com.au>
2019-01-26 00:53:33 +0000

    Add new test elements

---
+++ GGJ2019/Assets/Scripts/WanderAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderAI : MonoBehaviour
+{
+    public float wanderRadius;
+    public float wanderTimer;
+
+    private Transform target;
+    private UnityEngine.AI.NavMeshAgent agent;
+    private float timer;
+
+
+    private void OnEnable() {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > wanderTimer) {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+    
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask) {
+        Vector3 randomDirection = Random.insideUnitSphere * dist;
+
+        randomDirection += origin;
+
+        UnityEngine.AI.NavMeshHit navHit;
+
+        UnityEngine.AI.NavMesh.SamplePosition(randomDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}

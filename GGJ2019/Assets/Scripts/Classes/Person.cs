d7ca30e4b6e0e1ade52039f89a5e21da8c19fd96
benxtan <benxtan@gmail.com>
2019-01-26 12:08:50 +0000

    person stuff

--- GGJ2019/Assets/Scripts/Classes/Person.cs
+++ GGJ2019/Assets/Scripts/Classes/Person.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Person : MonoBehaviour
 {
     public float health;
-    public string feeling;
+    public Feeling feeling;
+
+    public string feelingString
+    {
+        get
+        {
+            return feeling.ToString();
+        }
+        set
+        {
+
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }

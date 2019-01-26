95297655d88b018e1e3ac6a0f47dd16a97ff458d
Daniel <dev@salvagelabs.com.au>
2019-01-26 11:15:01 +0000

    Create folder inside Scripts, move Managers, work on Obstacle manager

--- GGJ2019/Assets/Scripts/Controls/UserInput.cs
+++ GGJ2019/Assets/Scripts/Controls/UserInput.cs
@@ -12,7 +12,6 @@ public class UserInput : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         MoveCamera();
-        // RotateCamera();
     }
  
     private void MoveCamera() {
@@ -38,11 +37,41 @@ public class UserInput : MonoBehaviour {
             movement.z -= ResourceManager.ScrollSpeed;
         }
  
+        //horizontal camera movement
+        if(xpos >= 0 && xpos < ResourceManager.ScrollWidth) {
+            movement.x -= ResourceManager.ScrollSpeed;
+        } else if(xpos <= Screen.width && xpos > Screen.width - ResourceManager.ScrollWidth) {
+            movement.x += ResourceManager.ScrollSpeed;
+        }
+ 
+        //vertical camera movement
+        if(ypos >= 0 && ypos < ResourceManager.ScrollWidth) {
+            movement.z -= ResourceManager.ScrollSpeed;
+        } else if(ypos <= Screen.height && ypos > Screen.height - ResourceManager.ScrollWidth) {
+            movement.z += ResourceManager.ScrollSpeed;
+        }
+ 
+        //make sure movement is in the direction the camera is pointing
+        //but ignore the vertical tilt of the camera to get sensible scrolling
+        movement = Camera.main.transform.TransformDirection(movement);
+        movement.y = 0;
+ 
+        //away from ground movement
+        movement.y -= ResourceManager.ScrollSpeed * Input.GetAxis("Mouse ScrollWheel");
+ 
         //calculate desired camera position based on received input
         Vector3 origin = Camera.main.transform.position;
         Vector3 destination = origin;
         destination.x += movement.x;
+        destination.y += movement.y;
         destination.z += movement.z;
+ 
+        //limit away from ground movement to be between a minimum and maximum distance
+        if(destination.y > ResourceManager.MaxCameraHeight) {
+            destination.y = ResourceManager.MaxCameraHeight;
+        } else if(destination.y < ResourceManager.MinCameraHeight) {
+            destination.y = ResourceManager.MinCameraHeight;
+        }
  
         //if a change in position is detected perform the necessary update
         if(destination != origin) {
--- GGJ2019/Assets/Scripts/Managers/ResourceManager.cs
+++ GGJ2019/Assets/Scripts/Managers/ResourceManager.cs
@@ -5,7 +5,7 @@ using System.Collections;
 namespace RTS {
     public static class ResourceManager {
         public static int ScrollWidth { get { return 15; } }
-        public static float ScrollSpeed { get { return 15; } }
+        public static float ScrollSpeed { get { return 12; } }
         public static float RotateAmount { get { return 10; } }
         public static float RotateSpeed { get { return 100; } }
         public static float MinCameraHeight { get { return 10; } }

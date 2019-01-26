5036d5e12bec595789698ee3e85f0ff1d31a4509
benxtan <benxtan@gmail.com>
2019-01-26 11:13:54 +0000

    stuff

--- GGJ2019/Assets/Scripts/Managers/PeopleManager.cs
+++ GGJ2019/Assets/Scripts/Managers/PeopleManager.cs
@@ -2,25 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
+public class PeopleManager : MonoBehaviour
 {
-    public int currentLevel = 0;
+    public GameObject person;
 
     // Start is called before the first frame update
     void Start()
     {
-        InitLevel(1);
-    }
-
-    void InitLevel(int level)
-    {
-        currentLevel = 1;
-        PeopleManager.CreatePerson();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public static void CreatePerson()
+    {
+
     }
 }

dec847be1b48f0eab2d4f0ba772369899ece5bbd
benxtan <benxtan@gmail.com>
2019-01-26 11:00:37 +0000

    added buliding mgr

--- GGJ2019/Assets/Scripts/Managers/BuildingManager.cs
+++ GGJ2019/Assets/Scripts/Managers/BuildingManager.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class BuildingManager : MonoBehaviour
 {
+    public GameObject[] buildings;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitBuildings();
+    }
+
+    void InitBuildings()
+    {
+        buildings = GameObject.FindGameObjectsWithTag("Buildings");
+        Debug.Log("Num Buildings: " + buildings.Length);
+
+        // Assign feelings
+
     }
 
     // Update is called once per frame

0775cbfc6a3dcc30cbb079406fe3cd338f0f4733
Daniel <dev@salvagelabs.com.au>
2019-01-26 01:31:02 +0000

    Updating prefabs, adding some classes

---
+++ GGJ2019/Assets/Scripts/Classes/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    public string feeling;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

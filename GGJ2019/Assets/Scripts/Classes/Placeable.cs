4f8064d5ae8040ff8887d230a3679d513d93a58f
Daniel <dev@salvagelabs.com.au>
2019-01-27 11:41:06 +0000

    Add placeables manager and dog obstacle

---
+++ GGJ2019/Assets/Scripts/Classes/Placeable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeable : MonoBehaviour
+{
+    public float placeableDeathTime;
+    public float placeableTimer;
+    public string placeableName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.placeableTimer += Time.deltaTime;
+
+        if (this.placeableTimer > this.placeableDeathTime)
+        {
+            Destroy(this.gameObject);
+            this.placeableTimer = 0;
+        }
+    }
+}
